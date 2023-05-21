c74006373fad70a7fa437b78e2421974c795a6dd
Lukas Bahrenberg <31018566+LukasBahrenberg@users.noreply.github.com>
2023-05-21 21:48:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ load_dotenv()
 
 # install Rust
 subprocess.run('curl https://sh.rustup.rs -sSf | sh -s -- -y', shell=True)
-subprocess.run('cargo install --no-default-features --force cargo-make', shell=True)
 subprocess.run('export PATH=$PATH:~/.cargo/bin/', shell=True)
+subprocess.run('cargo install --no-default-features --force cargo-make', shell=True)
 
 # get user name
 user = getpass.getuser()
