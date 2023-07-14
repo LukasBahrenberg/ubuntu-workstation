e61072f2176bf3690d7b3e32f59dd3b4d266f31c
lukasbahrenberg <lukas.bahrenberg@gmail.com>
2023-07-14 15:46:56 +0200

    nodejs, conda, .env.example

--- os.py
+++ os.py
@@ -9,33 +9,38 @@ subprocess.run('apt update', shell=True)
 subprocess.run('apt upgrade -y', shell=True)
 
 # installations
+
+# install dev dependencies
 subprocess.run('apt install -y build-essential', shell=True)
 subprocess.run('apt install -y libssl-dev', shell=True)
 subprocess.run('apt install -y whois', shell=True)
 subprocess.run('apt-get install -y pkg-config', shell=True)
 
+# install fail2ban
 subprocess.run('apt install -y fail2ban', shell=True)
 subprocess.run('apt-get install -y git', shell=True)
 
+# install python stuff
 subprocess.run('apt-get install -y python3-pip', shell=True)
 subprocess.run('pip3 install requests', shell=True)
 subprocess.run('pip3 install python-dotenv', shell=True)
 
+# install docker 
 subprocess.run('apt install -y software-properties-common', shell=True)
 subprocess.run('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg', shell=True)
 subprocess.run('add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"', shell=True)
 subprocess.run('apt update', shell=True)
 subprocess.run('apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin', shell=True)
 
+# install snapd
 subprocess.run('apt install -y snapd', shell=True)
-subprocess.run('snap install go --classic', shell=True)
 
-# ufw
+# setup ufw
 subprocess.run('ufw logging on', shell=True)
 subprocess.run('ufw allow 22', shell=True) #ssh
 subprocess.run('ufw --force enable', shell=True)
 
-# fail2ban
+# setup fail2ban
 subprocess.run('systemctl enable fail2ban', shell=True)
 subprocess.run('systemctl start fail2ban', shell=True)
 subprocess.run('systemctl status fail2ban', shell=True)
--- setup.py
+++ setup.py
@@ -9,17 +9,30 @@ subprocess.run('sudo rm -rf /os.py', shell=True)
 # load environment variables
 load_dotenv()
 
-# install Rust
+# install rust
 subprocess.run('curl https://sh.rustup.rs -sSf | sh -s -- -y', shell=True)
 subprocess.run('source \"$HOME/.cargo/env\"', shell=True)
 subprocess.run('cargo install --no-default-features --force cargo-make', shell=True)
+
+# install current nodejs version
+subprocess.run('curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -', shell=True)    
+subprocess.run('apt-get update', shell=True)
+subprocess.run('apt-get install -y nodejs', shell=True)
+subprocess.run('apt-get install -y npm', shell=True)
+
+# install conda 
+subprocess.run('wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh', shell=True)
+subprocess.run('bash Miniconda3-latest-Linux-x86_64.sh -b', shell=True)
+subprocess.run('rm Miniconda3-latest-Linux-x86_64.sh', shell=True)
+
+# install go
+subprocess.run('snap install go --classic', shell=True)
 
 # get user name
 user = getpass.getuser()
 
 # reset permissions
 subprocess.run('sudo chown -R {}:sudo /home/{}'.format(user, user), shell=True)
-
 
 # assign env variables
 gituser = os.getenv('GITUSER')
