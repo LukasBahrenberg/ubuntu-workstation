1e95e1dbe0b9128d15a17bb80dd60a975eb3b50e
LukasBahrenberg <lukas.bahrenberg@gmail.com>
2023-07-14 17:42:19 +0200

    installation fixes

--- setup.py
+++ setup.py
@@ -1,29 +1,24 @@
 import subprocess
-import os
-from dotenv import load_dotenv
 import getpass
 
 # delete setup script
 subprocess.run('sudo rm -rf /os.py', shell=True)
 
-# load environment variables
-load_dotenv()
-
 # install rust
 subprocess.run('curl https://sh.rustup.rs -sSf | sh -s -- -y', shell=True)
 subprocess.run('source \"$HOME/.cargo/env\"', shell=True)
-subprocess.run('cargo install --no-default-features --force cargo-make', shell=True)
+subprocess.run('cargo install --no-default-features --force cargo-make', shell=True) # does not seem to work
 
 # install current nodejs version
 subprocess.run('curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -', shell=True)    
-subprocess.run('apt-get update', shell=True)
-subprocess.run('apt-get install -y nodejs', shell=True)
-subprocess.run('apt-get install -y npm', shell=True)
+subprocess.run('sudo apt-get update', shell=True)
+subprocess.run('sudo apt-get install -y nodejs', shell=True)
 
 # install conda 
 subprocess.run('wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh', shell=True)
 subprocess.run('bash Miniconda3-latest-Linux-x86_64.sh -b', shell=True)
 subprocess.run('rm Miniconda3-latest-Linux-x86_64.sh', shell=True)
+subprocess.run('conda config --set auto_activate_base false', shell=True)
 
 # install go
 subprocess.run('snap install go --classic', shell=True)
@@ -35,8 +30,8 @@ user = getpass.getuser()
 subprocess.run('sudo chown -R {}:sudo /home/{}'.format(user, user), shell=True)
 
 # assign env variables
-gituser = os.getenv('GITUSER')
-gitemail = os.getenv('GITEMAIL')
+gituser = input("Enter GITUSER: ")
+gitemail = input("Enter GITEMAIL: ")
 
 # setup git
 subprocess.run('ssh-keygen -t ed25519 -C \"{}\"'.format(gitemail), shell=True)
